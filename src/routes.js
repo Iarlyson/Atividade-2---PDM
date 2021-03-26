f5057f32373006e3d62a65f6da4b83c5bdf35fc1
daniel-dantas <daniel.dantas.ip19@hotmail.com>
2021-03-25 21:48:17 -0300

    adicionando rota para o controller NPS

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ const { Router } = require("express");
 const StudentController = require("./controllers/StudentController");
 const SurveyController = require("./controllers/SurveyController");
 const SubjectsController = require("./controllers/SubjectController");
+const NPSController = require("./controllers/NPScontroller");
 
 const router = Router();
 
@@ -10,11 +11,12 @@ router.post("/discentes", StudentController.create);
 router.get("/discentes", StudentController.read);
 router.get("/discentes/:id", StudentController.get);
 
-
 // Survey Routes
 router.post("/surveys", SurveyController.create);
 
 // Subject Routes
 router.post("/subjects", SubjectsController.create);
+
+router.post("/nps/:survey_id", NPSController.execute);
 
 module.exports = router;
